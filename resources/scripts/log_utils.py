171b7f534507b9d5fea963e90dfe6663c0b79465
Lucas Marinoto <lucas_marinoto@msn.com>
2024-06-16 22:09:05 +0000

    removido codigos

--- resources/scripts/log_utils.py
+++ resources/scripts/log_utils.py
@@ -6,17 +6,9 @@ def setup_logger():
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
     return logging.getLogger()
 
-def log_info(message):
-    logger = logging.getLogger()
-    logger.info("[INFO] " + message)
-
-def log_warning(message):
-    logger = logging.getLogger()
-    logger.warning("[WARNING] " + message)
-
 def log_error(message):
     logger = logging.getLogger()
-    logger.error("[ERROR] " + message)
+    logger.error(message)
 
 def log_error_and_exit(message):
     log_error(message)
