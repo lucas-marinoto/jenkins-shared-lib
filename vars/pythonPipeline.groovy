87da20c1cd1ce928666d100387f32082255b8ff1
Lucas Marinoto <lucas_marinoto@msn.com>
2024-06-16 19:23:25 +0000

    add leitura arquivos dinamicos

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -13,19 +13,22 @@
 
 // vars/pythonPipeline.groovy
 
+// vars/pythonPipeline.groovy
+
 def call(String dockerImage, String scriptName) {
     def scriptsDir = 'scripts'
     def workspaceDir = pwd()
 
     // Cria o diretório de scripts no workspace
     sh "mkdir -p ${workspaceDir}/${scriptsDir}"
-    sh "ls -la ${workspaceDir}/${scriptsDir}"
+
+    // Lista dos scripts a serem copiados
+    def scriptFiles = ['example.py', 'log_utils.py']  // Adicione mais scripts conforme necessário
 
     // Copia todos os scripts Python da Shared Library para o workspace do Jenkins
-    libraryResource("scripts").each { script ->
+    scriptFiles.each { script ->
         writeFile file: "${workspaceDir}/${scriptsDir}/${script}", text: libraryResource("scripts/${script}")
     }
-    sh "ls -la ${workspaceDir}/${scriptsDir}"
 
     // Executa o script Python dentro do container Docker, montando o diretório de scripts como volume
     def result = sh script: "docker run --rm -v ${workspaceDir}/${scriptsDir}:/app -w /app ${dockerImage} python ${scriptName}", returnStatus: true
