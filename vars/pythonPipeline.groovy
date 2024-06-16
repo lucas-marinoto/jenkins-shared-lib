af5d6d8ccd3d140b79b4fb9d2af69734ffa8c143
Lucas Marinoto <lucas_marinoto@msn.com>
2024-06-16 15:12:08 +0000

    add cat

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -3,6 +3,7 @@
 def call(String dockerImage, String scriptName) {
     // Copia o script Python da Shared Library para o workspace do Jenkins
     writeFile file: scriptName, text: libraryResource("scripts/${scriptName}")
+    sh "cat ${scriptName}"
 
     // Executa o script Python dentro do container Docker
     sh "docker run --rm -v ${pwd()}:/app -w /app ${dockerImage} python ${scriptName}"
